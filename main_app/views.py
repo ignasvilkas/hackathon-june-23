607391aac579af0ed2d0ce9bd801b363652bbd2f
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-06-15 10:12:25 +0000

    Create: Files on main directory & view for index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_page(request):
+    """
+    When this view is called it takes the request and return the
+    response which will render home page.
+    """
+    template = 'main_app/index.html'
+    return render(request, template)
