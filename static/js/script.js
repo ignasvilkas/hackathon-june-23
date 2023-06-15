7ef26a09d729f5ec923ec3588603782cdd88e99b
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-06-15 10:41:48 +0000

    Test JS link & add instructive comments

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// Frontend Developers here you will write all the code for JavaScript.
+// Delete the placeholder below and start coding. 
+// Godd Luck!
+
+function changeButton() {
+    var button = document.querySelector('.my-button');
+    
+    // Change color
+    button.style.backgroundColor = 'red';
+    
+    // Change text
+    button.innerHTML = 'Javascript works :)';
+}
