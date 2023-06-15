b5a8333aa77af12028c37df60282a5d24bfd64fd
Edmir-Demaj <edmir_demaj@yahoo.com>
2023-06-15 10:59:03 +0000

    Created: Other pages and linked them on navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('history/', views.history_page, name='history_page'),
+    path('resources/', views.resources_page, name='resources_page'),
+    path('contact/', views.contact_page, name='contact_page'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,30 @@ def home_page(request):
     """
     template = 'main_app/index.html'
     return render(request, template)
+
+
+def history_page(request):
+    """
+    When this view is called it takes the request and return the
+    response which will render history page.
+    """
+    template = 'main_app/history.html'
+    return render(request, template)
+
+
+def resources_page(request):
+    """
+    When this view is called it takes the request and return the
+    response which will render resources page.
+    """
+    template = 'main_app/resources.html'
+    return render(request, template)
+
+
+def contact_page(request):
+    """
+    When this view is called it takes the request and return the
+    response which will render contact page.
+    """
+    template = 'main_app/contact.html'
+    return render(request, template)
