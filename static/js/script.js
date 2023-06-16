6fe761692205252888418fd8d0fa9b59d867b1e2
Jody M <jodyamm@hotmail.com>
2023-06-16 08:19:14 +0000

    Edit: Work on smaller screen size menu for navbar

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,23 @@
 // Frontend Developers here you will write all the code for JavaScript.
-// Delete the placeholder below and start coding. 
+// Delete the placeholder below and start coding.
 // Godd Luck!
 
 function changeButton() {
-    var button = document.querySelector('.my-button');
-    
-    // Change color
-    button.style.backgroundColor = 'red';
-    
-    // Change text
-    button.innerHTML = 'Javascript works :)';
+  var button = document.querySelector(".my-button");
+
+  // Change color
+  button.style.backgroundColor = "red";
+
+  // Change text
+  button.innerHTML = "Javascript works :)";
 }
-  
+
+function closeSideNav() {
+    var sidenavInstance = M.Sidenav.getInstance(document.getElementById('mobile-nav'));
+    sidenavInstance.close();
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
